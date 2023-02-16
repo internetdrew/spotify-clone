8a1f60003a0f86ebc1026171fae6e3211d569c9b
internetdrew <andrew@internetdrew.com>
2023-02-16 08:23:20 -0500

    add sidebar component

---
+++ components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <div>
+      <h1>I am a sidebar</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
-import styles from '@/styles/Home.module.css';
+import Sidebar from '@/components/Sidebar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,7 +20,7 @@ export default function Home() {
       <h1>Spotify</h1>
 
       <main>
-        {/* sidebar */}
+        <Sidebar />
         {/* center */}
       </main>
 
