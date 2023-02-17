d2b07ae89591eb09b051ef70b8587189895fb434
internetdrew <andrew@internetdrew.com>
2023-02-16 22:22:07 -0500

    fix middleware execution from depracated method

--- middleware.js
+++ middleware.js
@@ -1,7 +1,12 @@
 import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
 
+export const config = {
+  matcher: '/',
+};
+
 export async function middleware(req) {
+  console.log('middleware');
   const token = await getToken({ req, secret: process.env.JWT_SECRET });
 
   const { pathname } = req.nextUrl;
@@ -10,11 +15,15 @@ export async function middleware(req) {
   // 1. it's a request for for next-auth session &  provider fetching
   // 2. the token exists
   if (pathname.includes('/api/auth') || token) {
+    console.log('auth route accessed');
     return NextResponse.next();
   }
 
   // redirect the user on token fail && request of protected route
   if (!token && pathname !== '/login') {
-    return NextResponse.redirect('/login');
+    console.log('no user token, access denied');
+    const url = req.nextUrl.clone();
+    url.pathname = '/login';
+    return NextResponse.redirect(url);
   }
 }
