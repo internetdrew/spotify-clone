340214a752e64eddd0cafed530698db850b5c027
internetdrew <andrew@internetdrew.com>
2023-02-16 21:55:53 -0500

    move middleware to root

---
+++ _middleware.js
@@ -0,0 +1,20 @@
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
+
+export async function middleware(req) {
+  const token = await getToken({ req, secret: process.env.JWT_SECRET });
+
+  const { pathname } = req.nextUrl;
+
+  // Allow requests if the following is true...
+  // 1. it's a request for for next-auth session &  provider fetching
+  // 2. the token exists
+  if (pathname.includes('/api/auth') || token) {
+    return NextResponse.next();
+  }
+
+  // redirect the user on token fail && request of protected route
+  if (!token && pathname !== '/login') {
+    return NextResponse.redirect('/login');
+  }
+}
