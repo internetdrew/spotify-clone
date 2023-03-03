cbc870893c9ba7e713c15d8b8831c3cf44156dc0
internetdrew <andrew@internetdrew.com>
2023-03-03 01:02:43 -0500

    start building hook for track info

---
+++ components/Player.jsx
@@ -0,0 +1,25 @@
+import useSpotify from '@/hooks/useSpotify';
+import { useSession } from 'next-auth/react';
+import React, { useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { currentTrackIdState, isPlayingState } from '@/atoms/songAtom';
+
+const Player = () => {
+  const spotifyApi = useSpotify();
+  const { data: session, status } = useSession();
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+  const [volume, setVolume] = useState(50);
+
+  return (
+    <div>
+      {/* left */}
+      <div>
+        <img src='' alt='' />
+      </div>
+    </div>
+  );
+};
+
+export default Player;
---
+++ hooks/useSongInfo.js
@@ -0,0 +1,27 @@
+import { currentTrackIdState } from '@/atoms/songAtom';
+import { useRecoilState } from 'recoil';
+import useSpotify from './useSpotify';
+import { useState, useEffect } from 'react';
+
+function useSongInfo() {
+  const spotifyApi = useSpotify();
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
+  const [songInfo, setSongInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchSongInfo = async () => {
+      if (currentTrackId) {
+        const trackInfo = await fetch(
+          `https://api.spotify.com/v1/tracks/${currentTrackId}`
+        );
+      }
+    };
+
+    fetchSongInfo();
+  }, [currentTrackId, spotifyApi]);
+
+  return songInfo;
+}
+
+export default useSongInfo;
